d5a2d81ecdbf89177bc4db75192c258c2d331dc3
junaja <jomblangan27@gmail.com>
2019-05-31 21:18:43 +0700

    adding most of things i need in building new project

--- helper/src/main/java/com/ian/app/helper/util/LoadImageHelper.kt
+++ helper/src/main/java/com/ian/app/helper/util/LoadImageHelper.kt
@@ -1,6 +1,11 @@
 package com.ian.app.helper.util
 
+import android.content.Context
 import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.signature.ObjectKey
 import com.ian.app.helper.R
 import com.squareup.picasso.Picasso
 
@@ -19,3 +24,15 @@ fun ImageView.loadUrlResize(url: String?) {
     url?.let { Picasso.get().load(it).placeholder(R.drawable.empty_image).resize(100, 100).into(this) }
 
 }
+
+fun ImageView.loadResizeWithGlide(url:String?, context: Context){
+    val requestOptions = RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)
+        .override(150,150)
+    Glide.with(context).load(url).apply(requestOptions).placeholder(R.drawable.empty_image).thumbnail(0.25f).into(this)
+}
+
+fun ImageView.loadWithGlide(url:String?, context: Context){
+    val requestOptions = RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)
+    Glide.with(context).load(url).apply(requestOptions).placeholder(R.drawable.empty_image).thumbnail(0.25f).into(this)
+}
+
