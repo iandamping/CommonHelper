7c308bd77edd977eae2bdc2ef7dd67041f475946
junaja <jomblangan27@gmail.com>
2019-06-03 16:46:36 +0700

    Adding combine in coroutine helper

--- helper/src/main/java/com/ian/app/helper/util/CoroutineHelper.kt
+++ helper/src/main/java/com/ian/app/helper/util/CoroutineHelper.kt
@@ -1,6 +1,5 @@
 package com.ian.app.helper.util
 
-import com.ian.app.helper.R
 import kotlinx.coroutines.*
 
 /**
@@ -74,6 +73,61 @@ inline fun <reified T, U, S, K> CoroutineScope.combineTripleWithSingleDeferred(
                         deferredSource1.first.third.await()
                     ),
                     deferredSource1.second.await()
+                )
+            )
+        } catch (t: Throwable) {
+            onFailed(t.localizedMessage)
+        }
+    }
+}
+
+inline fun <reified L, Y, N, D, A> CoroutineScope.combineTripleWithPairDeferred(
+    deferredSource1: Pair<Triple<Deferred<L>, Deferred<Y>, Deferred<N>>, Pair<Deferred<D>, Deferred<A>>>,
+    crossinline onSuccess: (Pair<Triple<L, Y, N>, Pair<D, A>>) -> Unit,
+    crossinline onFailed: (String) -> Unit
+) {
+    this.launch {
+        try {
+            onSuccess(
+                Pair(
+                    Triple(
+                        deferredSource1.first.first.await(),
+                        deferredSource1.first.second.await(),
+                        deferredSource1.first.third.await()
+                    ),
+                    Pair(
+                        deferredSource1.second.first.await(),
+                        deferredSource1.second.second.await()
+                    )
+
+                )
+            )
+        } catch (t: Throwable) {
+            onFailed(t.localizedMessage)
+        }
+    }
+}
+
+inline fun <reified R, O, D, I, T, H> CoroutineScope.combineTripleWithTripleDeferred(
+    deferredSource1: Pair<Triple<Deferred<R>, Deferred<O>, Deferred<D>>, Triple<Deferred<I>, Deferred<T>, Deferred<H>>>,
+    crossinline onSuccess: (Pair<Triple<R, O, D>, Triple<I, T, H>>) -> Unit,
+    crossinline onFailed: (String) -> Unit
+) {
+    this.launch {
+        try {
+            onSuccess(
+                Pair(
+                    Triple(
+                        deferredSource1.first.first.await(),
+                        deferredSource1.first.second.await(),
+                        deferredSource1.first.third.await()
+                    ),
+                    Triple(
+                        deferredSource1.second.first.await(),
+                        deferredSource1.second.second.await(),
+                        deferredSource1.second.third.await()
+                    )
+
                 )
             )
         } catch (t: Throwable) {
