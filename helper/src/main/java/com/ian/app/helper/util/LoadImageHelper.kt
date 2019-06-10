d537a815cb3e5ee94e902513277c093740ba30fe
junaja <jomblangan27@gmail.com>
2019-06-10 16:11:21 +0700

    fixing context in loadimage helper with glide

--- helper/src/main/java/com/ian/app/helper/util/LoadImageHelper.kt
+++ helper/src/main/java/com/ian/app/helper/util/LoadImageHelper.kt
@@ -1,6 +1,5 @@
 package com.ian.app.helper.util
 
-import android.content.Context
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -13,14 +12,16 @@ Created by Ian Damping on 06/05/2019.
 Github = https://github.com/iandamping
  */
 
-fun ImageView.loadResizeWithGlide(url: String?, context: Context) {
+fun ImageView.loadResizeWithGlide(url: String?) {
     val requestOptions = RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)
         .override(150, 150)
-    Glide.with(context).load(url).apply(requestOptions).placeholder(R.drawable.empty_image).thumbnail(0.25f).into(this)
+    Glide.with(this.context).load(url).apply(requestOptions).placeholder(R.drawable.empty_image).thumbnail(0.25f)
+        .into(this)
 }
 
-fun ImageView.loadWithGlide(url: String?, context: Context) {
+fun ImageView.loadWithGlide(url: String?) {
     val requestOptions = RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)
-    Glide.with(context).load(url).apply(requestOptions).placeholder(R.drawable.empty_image).thumbnail(0.25f).into(this)
+    Glide.with(this.context).load(url).apply(requestOptions).placeholder(R.drawable.empty_image).thumbnail(0.25f)
+        .into(this)
 }
 
