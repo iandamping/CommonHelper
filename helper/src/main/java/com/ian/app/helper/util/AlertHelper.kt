9170a7a300307d0d0941d04a1484ce24e2bb5dfb
junaja <jomblangan27@gmail.com>
2019-06-11 13:19:24 +0700

    adding simple alerthelper

--- helper/src/main/java/com/ian/app/helper/util/AlertHelper.kt
+++ helper/src/main/java/com/ian/app/helper/util/AlertHelper.kt
@@ -2,6 +2,7 @@ package com.ian.app.helper.util
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AlertDialog
 import com.ian.app.helper.R
 import kotlinx.android.synthetic.main.custom_failed_dialog.view.*
@@ -12,14 +13,17 @@ Created by Ian Damping on 11/06/2019.
 Github = https://github.com/iandamping
  */
 
-inline fun<reified T : Activity> Context.alertHelperFailed(tittle: String?) {
+inline fun<reified T : Activity> Context.alertHelperFailed(tittle: String?,noinline init: Intent.() -> Unit = {}) {
+    val intent = newIntent<T>(this)
+    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+    intent.init()
     val dialogBuilder = AlertDialog.Builder(this)
     val inflater = this.layoutInflater
     val dialogView = inflater.inflate(R.layout.custom_failed_dialog, null)
     dialogView.tvAlertMessage.text = tittle
     dialogBuilder.setPositiveButton("Oke") { dialog, _ ->
         dialog.dismiss()
-        this@alertHelperFailed.startActivity<T>()
+        startActivity(intent)
     }
 
     dialogBuilder.setView(dialogView)
@@ -29,14 +33,17 @@ inline fun<reified T : Activity> Context.alertHelperFailed(tittle: String?) {
 
 }
 
-inline fun <reified T : Activity> Context.alertHelperSuccess(tittle: String?) {
+inline fun <reified T : Activity> Context.alertHelperSuccess(tittle: String?,noinline init: Intent.() -> Unit = {}) {
+    val intent = newIntent<T>(this)
+    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+    intent.init()
     val dialogBuilder = AlertDialog.Builder(this)
     val inflater = this.layoutInflater
     val dialogView = inflater.inflate(R.layout.custom_failed_dialog, null)
     dialogView.tvAlertMessage.text = tittle
     dialogBuilder.setPositiveButton("Oke") { dialog, _ ->
         dialog.dismiss()
-        this@alertHelperSuccess.startActivity<T>()
+        startActivity(intent)
     }
 
     dialogBuilder.setView(dialogView)
