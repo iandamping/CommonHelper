5788ebc0d38ea1ac5bfd509d7591a534b352a975
junaja <jomblangan27@gmail.com>
2019-10-04 11:06:04 +0700

    adding intent to open web

--- helper/src/main/java/com/ian/app/helper/util/IntentHelper.kt
+++ helper/src/main/java/com/ian/app/helper/util/IntentHelper.kt
@@ -3,10 +3,21 @@ package com.ian.app.helper.util
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 /**
  *
@@ -41,3 +52,10 @@ fun FragmentManager.switchFragment(savedInstanceState: Bundle?, layoutResId: Int
         this.beginTransaction().replace(layoutResId, target).commit()
     }
 }
+
+fun intentOpenWebsite(activity: Activity, url: String) {
+    val openURL = Intent(Intent.ACTION_VIEW)
+    openURL.data = Uri.parse(url)
+    activity.startActivity(openURL)
+}
+
